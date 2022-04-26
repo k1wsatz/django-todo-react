fa4091fa94a3a0d33ce4967003baf31dc0caa57d
wnumik <k1wsatz@wnumiks-MacBook-Pro.local>
2022-04-26 13:46:20 +0200

    completed todo backend with rest framework

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
+
 
 # Register your models here.
+admin.site.register(models.Todo, TodoAdmin)
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from . import serializers
+from . import models
+
 
 # Create your views here.
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = serializers.TodoSerializer
+    queryset = models.Todo.objects.all()
